3ae78f44e58fbca14c75467d7b775ffb24e9e5d1
grund <grundy_j@live.com>
2018-10-31 11:48:56 -0700

    initial commit

---
+++ TwentyOneGameExercise/Casino/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public abstract class Game
+    {
+        private List<Player> _Players = new List<Player>();
+        private Dictionary<Player, int> _Bets = new Dictionary<Player, int>();
+
+        public List<Player> Players { get { return _Players; } set { _Players = value; } }
+        public string Name { get; set; }
+        public Dictionary<Player, int> Bets { get { return _Bets; } set { _Bets = value; } }
+
+        public abstract void Play();
+
+        public virtual void ListPlayers()
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+        }
+    }
+}
